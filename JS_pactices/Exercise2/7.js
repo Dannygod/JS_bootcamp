82e20a9acaf59ce2cc8c1ad1d02cf726bafbcd5d
Danny Hsu <danny539425@gmail.com>
2023-02-09 11:52:16 +0800

    ex2-7

---
+++ JS_pactices/Exercise2/7.js
@@ -0,0 +1,9 @@
+function reverse(str) {
+  let newStr = "";
+  for (let i = str.length - 1; i >= 0; i++) {
+    newStr += str[i];
+  }
+  console.log(newStr);
+}
+reverse("abcd"); // returns "dcba"
+reverse("I am a good guy."); // returns ".yug doog a am I"
