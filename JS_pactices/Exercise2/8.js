2187e69ed687c7ab625b162f964df30e71093e78
Danny Hsu <danny539425@gmail.com>
2023-02-09 12:03:26 +0800

    ex2-8

---
+++ JS_pactices/Exercise2/8.js
@@ -0,0 +1,13 @@
+function swap(str) {
+  let newStr = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == str[i].toUpperCase) {
+      newStr += str[i].toLowerCase();
+    } else {
+      newStr += str[i].toUpperCase();
+    }
+  }
+  return newStr;
+}
+console.log(swap("Aloha")); // returns "aLOHA"
+console.log(swap("Love you.")); // returns "lOVE YOU."
