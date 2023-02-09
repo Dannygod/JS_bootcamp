ccec7046c14b067cf8738c3851d278dcd6b6b61e
Danny Hsu <danny539425@gmail.com>
2023-02-09 11:53:49 +0800

    ex2-7

--- JS_pactices/Exercise2/7.js
+++ JS_pactices/Exercise2/7.js
@@ -1,6 +1,6 @@
 function reverse(str) {
   let newStr = "";
-  for (let i = str.length - 1; i >= 0; i++) {
+  for (let i = str.length - 1; i >= 0; i--) {
     newStr += str[i];
   }
   console.log(newStr);
