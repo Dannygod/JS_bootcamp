821426493ccb665b6acffcf549d076f68d701049
Danny Hsu <danny539425@gmail.com>
2023-02-10 11:09:59 +0800

    ex4-3

--- JS_pactices/Exercise4/3.js
+++ JS_pactices/Exercise4/3.js
@@ -1,3 +1,17 @@
-function flatten(arr) {}
-flatten([1, [[], 2, [0, [1]], [3]], [1, 3, [3], [4, [1]], [2]]]);
+function flatten(a1) {
+  let a2 = [];
+  help(a1);
+  return a2;
+
+  function help(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+        help(arr[i]);
+      } else {
+        a2.push(arr[i]);
+      }
+    }
+  }
+}
+console.log(flatten([1, [[], 2, [0, [1]], [3]], [1, 3, [3], [4, [1]], [2]]]));
 // returns [1, 2, 0, 1, 3, 1, 3, 3, 4, 1, 2]
