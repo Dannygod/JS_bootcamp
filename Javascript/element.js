61db5e1d297f12ea4a6c2a6d55f9e1508a9433fb
Danny Hsu <danny539425@gmail.com>
2023-02-10 14:54:56 +0800

    element

--- Javascript/element.js
+++ Javascript/element.js
@@ -1,2 +1,7 @@
 let body = document.querySelector("body");
-console.log(body);
+console.log(body.childNodes);
+console.log(body.children);
+
+let myP = document.querySelector("p.second");
+
+console.log(myP.parentNode.parentNode);
---
+++ Javascript/getElementById.js
@@ -0,0 +1,5 @@
+let myH1 = document.getElementById("first");
+let myP = document.getElementById("second");
+console.log(window);
+console.log(myH1);
+console.log(myP);
