f84a606bd93c23541e9663ec83223bf658a738df
Danny Hsu <danny539425@gmail.com>
2023-02-10 10:32:13 +0800

    ex4-1

---
+++ JS_pactices/Exercise4/2.js
@@ -0,0 +1,15 @@
+function intersection(a1, a2) {
+  let a3 = [];
+  let find = 0;
+  for (let i = 0; i < a1.length; i++) {
+    find = a1[i];
+    for (let j = 0; j < a2.length; j++) {
+      if (find == a2[j]) {
+        a3.push(find);
+      }
+    }
+  }
+  return a3;
+}
+console.log(intersection([1, 3, 4, 6, 10], [5, 11, 4, 3, 100, 144, 0])); // returns [3, 4]
+console.log(intersection([0], [5, 11, 4, 3, 100, 44])); // returns [3, 4]
