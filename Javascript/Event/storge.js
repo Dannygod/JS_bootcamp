b4f979af0664ea1927410b102a57ff34798b9fb5
Danny Hsu <danny539425@gmail.com>
2023-02-13 15:24:30 +0800

    storage-1

--- Javascript/Event/storge.js
+++ Javascript/Event/storge.js
@@ -1,5 +1,11 @@
 localStorage.setItem("name", "Danny");
 localStorage.setItem("age", "18");
 
+let myFriends = ["Terry", "May", "Zoe"];
+localStorage.setItem("friend", myFriends);
+
 let myName = localStorage.getItem("name");
 console.log(myName);
+
+console.log(typeof myFriends);
+console.log(typeof localStorage.friend);
