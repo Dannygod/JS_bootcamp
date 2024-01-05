1db5a7bbc42c132d8d621438c142b4e2b145d236
Danny Hsu <danny539425@gmail.com>
2024-01-05 11:34:39 +0800

    1/5

---
+++ Nodejs/mymath.js
@@ -0,0 +1,14 @@
+const add = (a, b) => {
+    return a+b;
+}
+function minus(a, b){
+    return a-b;
+}
+function multiple(a, b){
+    return a*b;
+}
+function divide(a, b){
+    return a/b;
+}
+
+modules.exports = {add, minus, multiple, divide};
