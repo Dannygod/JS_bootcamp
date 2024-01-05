367e1dfc50e3fb7a75e9aa7c029152d97f94e676
Danny Hsu <danny539425@gmail.com>
2024-01-05 14:40:22 +0800

    Update url.js

---
+++ url.js
@@ -0,0 +1,3 @@
+const url = require(url);
+let myurl = 'http://127.0.0.1:5502/LV7/LV7.5-datalist.html?name=%E8%A8%B1%E5%AE%B8%E7%A0%94&email=danny539425%40gmail.com&food=%E5%86%B0%E6%B7%87%E6%B7%8B&password=123456789'
+
