252c15b2df80f8cfac6be52314d517400cd2305d
Danny Hsu <danny539425@gmail.com>
2024-01-08 09:54:09 +0800

    0108

---
+++ Nodejs/Cowsay/app.js
@@ -0,0 +1,7 @@
+const cowsay = require("cowsay");
+
+console.log(cowsay.think({
+    text : "This is baby cow.",
+    e : "v_V",
+    T : "J "
+}));
---
+++ Nodejs/Cowsay/cowsay.js
@@ -0,0 +1,8 @@
+import { think, SQUIRREL } from 'cowsay';
+
+console.log(think({
+  text: 'grazing in the browser',
+  cow: SQUIRREL,
+  eyes: 'pp',
+  tongue: ';;',
+}));
