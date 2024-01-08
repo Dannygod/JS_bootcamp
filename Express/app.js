b479d85c90638eeda6b21868e34e1f44a35b0e1b
Danny Hsu <danny539425@gmail.com>
2024-01-08 14:39:14 +0800

    fruit routing

--- Express/app.js
+++ Express/app.js
@@ -1,12 +1,30 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 app.get("/", (req, res) => {
-    // console.log(req);
-    res.send("網頁 首頁");
-    // res.send("The First page");
-    res.end();
-})
-
+    console.log(__dirname, "index.html");
+    res.sendFile(path.join(__dirname, "index.html")); // Use path.join to construct the file path
+});
+app.get("/p1", (req, res) => {
+    res.send("網頁");
+});
+app.get("/p2", (req, res) => {
+    res.send("網頁2");
+});
+app.get("/fruit/:kindOfFruit", (req, res) => {
+    const {kindOfFruit} = req.params;
+    const fruitList = ["apple", "banana", "guava"]
+    fruitList.forEach(i => {
+        if (kindOfFruit === i){
+            res.send(`Fruit you are looking for: ${kindOfFruit}`);
+        }
+    });
+    res.send(`Fruit you are looking for: Not found`);
+});
+app.get("/:errorPage", (req, res) => {
+    console.log(req.params);
+    res.send(`The page ${req.params.errorPage} is not exist!`);
+}); // error page
 app.listen(9288, () => {
-    console.log("App is running at 9288");
-})
+    console.log("App is running at https://localhost:9288");
+});
