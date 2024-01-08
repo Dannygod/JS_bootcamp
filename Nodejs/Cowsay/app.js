d60c3fcb81a0287d721e7e6a520858ffc1c6491b
Danny Hsu <danny539425@gmail.com>
2024-01-08 11:21:43 +0800

    add script in package

--- Nodejs/Cowsay/app.js
+++ Nodejs/Cowsay/app.js
@@ -2,6 +2,6 @@ const cowsay = require("cowsay");
 
 console.log(cowsay.think({
     text : "This is baby cow.",
-    e : "v_V",
+    e : "v_7",
     T : "J "
 }));
--- Nodejs/Cowsay/cowsay.js
+++ Nodejs/Cowsay/cowsay.js
@@ -6,3 +6,4 @@ console.log(think({
   eyes: 'pp',
   tongue: ';;',
 }));
+
