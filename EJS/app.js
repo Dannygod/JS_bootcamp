a3a0bb734495ca3826422ad56c96a5826d62e95d
Danny Hsu <41147020S@gapps.ntnu.edu.tw>
2024-01-09 14:37:15 +0800

    0108

--- EJS/app.js
+++ EJS/app.js
@@ -1,7 +1,16 @@
-const {express} = require('express');
+const express = require('express');
 const app = express();
-app
+// middleware
+app.use(express.static("public"));
 
-app.listen(6060, () => {
-    console.log("");
+app.get('/', (req, res) => {
+    res.send("This is home page");
+});
+app.get('/:name', (req, res) => {
+    const { name } = req.params;
+    res.render('index.ejs', { name });
+});
+const port = 6060;
+app.listen(port, () => {
+    console.log(`You are at port ${port}`);
 })
