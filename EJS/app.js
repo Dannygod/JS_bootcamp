6d326cf5aa60d95fa7c288c56e1d2a3f387c80b0
Danny Hsu <41147020S@gapps.ntnu.edu.tw>
2024-01-09 15:34:10 +0800

    0109 ejs form

--- EJS/app.js
+++ EJS/app.js
@@ -6,6 +6,13 @@ app.use(express.static("public"));
 app.get('/', (req, res) => {
     res.render('index.ejs');
 });
+app.get('/form', (req, res) => {
+    res.render('form.ejs');
+});
+app.get('/response', (req, res) => {
+    const { username, userage } = req.query;
+    res.render('form_confirm.ejs', { username: username, userage: userage });
+});
 app.get('/:name', (req, res) => {
     const { name } = req.params;
     res.render('person.ejs', { name: name }); // {name} is same as {name:name}
