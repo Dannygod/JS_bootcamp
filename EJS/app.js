292d90659f84dcfc96b3415c879ff6c8e8fe3fd7
Danny Hsu <41147020S@gapps.ntnu.edu.tw>
2024-01-10 13:32:40 +0800

    0110 ejs add data

--- EJS/app.js
+++ EJS/app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
 // middleware
-app.use(express.static("public"));
 
+app.use(express.static("public"));
+let languages = [
+    {name: "HTML", rate: 9.7, popularity: 1, trending: "super hot"},
+    {name: "CSS", rate: 9.5, popularity: 2, trending: "same"}, 
+    {name: "JavaScript", rate: 9.9, popularity: 3, trending: "super hot"},
+    {name: "Python", rate: 9.9, popularity: 4, trending: "super hot"},
+    {name: "Java", rate: 9.6, popularity: 5, trending: "hot"},
+    {name: "C", rate: 9.5, popularity: 6, trending: "same"},
+];
 app.get('/', (req, res) => {
-    res.render('index.ejs');
+    res.render('index.ejs', {languages: languages});
 });
 app.get('/form', (req, res) => {
     res.render('form.ejs');
