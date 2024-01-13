043127bf01cf2b955df3ead3dfdb21426b2ad33d
Danny Hsu <41147020S@gapps.ntnu.edu.tw>
2024-01-13 11:14:23 +0800

    Update app.js

--- Mogoose/app.js
+++ Mogoose/app.js
@@ -13,30 +13,43 @@ mongoose.connect('mongodb://localhost:27017/test', {
 })
 // define a schema
 const studentSchema = new mongoose.Schema({
-    name: String,
-    age: Number,
-    major: String,
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        requried: [true, "Age is required"]
+    },
+    major: {
+        type: String,
+        enum: ["Computer Science", "Math", "Physics"],
+    },
     scholarship: {
-        merit: Number,
-        other: Number
+        merit: {
+            type: Number,
+        },
+        other: {
+            type: Number,
+        }
     },
 });
 // create a model for students
 const Student = mongoose.model("Student", studentSchema);
 // save a Jon to database
-const Jon = new Student({
-    name: "Jon",
-    age: 18,
+const Kelly = new Student({
+    name: "Kelly",
+    age: Math.floor(Math.random() * 10) + 18,
     major: "Computer Science",
     scholarship: {
-        merit: 1000,
-        other: 500
+        merit: Math.floor(Math.random() * 1000) + 500,
+        other: Math.floor(Math.random() * 500) + 100
     }
 });
 // save Jon to DB
-// Jon.save()
+// Kelly.save()
 // .then(() =>{
-//     console.log("Jon has been saved to DB");
+//     console.log("Kelly has been saved to DB");
 // })
 // .catch((err) =>{
 //     console.log("Error happend when saving Jon to DB");
@@ -44,19 +57,29 @@ const Jon = new Student({
 // });
 
 // find Jon in DB
-Student.updateOne(
-    {name: "David"}, 
-    {name: "Cindy"}
-).then((mes) =>{
-    console.log(mes);
-});
+// Student.findOneAndUpdate(
+//     {name: "Eva"}, 
+//     {name: "Jenny"},
+//     {new: true}
+// ).then((mes) =>{
+//     console.log(mes);
+// });
+
 // update merit plus 500 to every student
 // Student.updateMany({}, {$inc: {"scholarship.merit": 500}}).then((data) =>{
 //     console.log(data);
 // });
-Student.find({}).then((data) =>{
+
+// find all students
+// Student.find({}).then((data) =>{
+//     console.log(data);
+// });
+
+// delete Jon
+Student.findOneAndDelete({name: "Kelly"}).then((data) => {
     console.log(data);
 });
+
 app.get('/', (req, res) =>{
     res.render('index.ejs');
 })
