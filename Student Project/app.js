d66480aebe31e56ffa5f7045d821a6795684a655
Danny Hsu <41147020S@gapps.ntnu.edu.tw>
2024-01-17 09:09:55 +0800

    0117 setup

---
+++ Student Project/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const { default: mongoose } = require('mongoose');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
+mongoose.connect("mongodb://localhost:27017/studentDB", {useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=>{
+    console.log("Connected to DB");
+})
+.catch((e)=>{
+    console.log("Connection failed");
+    console.log(e);
+})
+app.get('/', (req, res) =>{
+    res.render('index.ejs');
+})
+app.listen(3000, ()=>{
+    console.log('Server started on port 3000');
+})
