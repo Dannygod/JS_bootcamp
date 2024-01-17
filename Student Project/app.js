a97a2e688c171ba54e14d4c867a620fbe797cf70
Danny Hsu <41147020S@gapps.ntnu.edu.tw>
2024-01-17 09:42:00 +0800

    0117 POST

--- Student Project/app.js
+++ Student Project/app.js
@@ -18,6 +18,14 @@ mongoose.connect("mongodb://localhost:27017/studentDB", {useNewUrlParser: true, 
 app.get('/', (req, res) =>{
     res.render('index.ejs');
 })
+app.get('/student/add', (req, res) =>{
+    res.render('studentForm.ejs');
+})
+app.post("/student/add", (req, res)=>{
+    const {id, name, age, merit, other} = req.body;
+    console.log(req.body);
+    res.send("Thanks for posting");
+});
 app.listen(3000, ()=>{
     console.log('Server started on port 3000');
 })
