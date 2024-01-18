974d5e5c0e3df86b6407e8c6a69aa552ce88d273
Danny Hsu <41147020S@gapps.ntnu.edu.tw>
2024-01-18 10:11:38 +0800

    0118 hello

---
+++ Middleware/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+app.set("view engine", "ejs");
+// middlewares
+app.use(express.static("public"));
+const middleware = (req, res, next) => {
+  console.log("hello");
+  next();  
+}
+// mongoose
+//   .connect("mongodb://localhost:27017/test", {
+//     useFindAndModify: false,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("Connected to mongodb.");
+//   })
+//   .catch((e) => {
+//     console.log(e);
+//   });
+app.get("/", middleware, (req, res) =>{
+  res.send("Hello World");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000.");
+});
