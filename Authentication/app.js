bc662fead2390354d53b0a48d3c2e274d3b517ae
Danny Hsu <41147020S@gapps.ntnu.edu.tw>
2024-01-19 13:26:01 +0800

    0119 sign up

--- Authentication/app.js
+++ Authentication/app.js
@@ -21,10 +21,39 @@ app.get("/", (req, res) => {
 app.get("/signup", (req, res) => {
     res.render("signup");
 });
+app.post("/signup", async (req, res, next) => {
+    console.log(req.body);
+    const { username, password } = req.body;
+    const newUser = new User({ username, userpassword: password });
+    try{
+        await newUser.save()
+        .then(() =>{
+            res.send("Successfully signed up!");
+            console.log("User created!");
+        })
+        .catch((e) =>{
+            console.log(e);
+            next(e);
+        });
+    } catch(e){
+        next(e);
+    }
+});
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.post("/login", async (req, res, next) => {
+    
+});    
 app.get("/", (req, res) => {
     res.render('index');
 });
-
+// error handler
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).send("Something went wrong :(<br>" + err);
+});
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
 });
