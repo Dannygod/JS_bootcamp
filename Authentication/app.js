11ec5d00ff0e8522e4696d82c970804ce8e5a873
Danny Hsu <41147020S@gapps.ntnu.edu.tw>
2024-01-19 16:00:06 +0800

    0119 login session

--- Authentication/app.js
+++ Authentication/app.js
@@ -4,10 +4,16 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const User = require('./model/user');
 const bcrypt = require('bcrypt');
+const session = require('express-session');
 const saltRounds = 11;
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+}));
 mongoose.connect('mongodb://localhost:27017/user', {
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(() =>{
@@ -16,9 +22,22 @@ mongoose.connect('mongodb://localhost:27017/user', {
     console.log("Connect to MongoDB failed");
     console.log(err);
 });
+const requireLogin = (req, res, next) => {
+    if(!req.session.isVerified){
+        res.redirect("/login");
+    }
+    else{
+        next();
+    }
+}
 
 app.get("/", (req, res) => {
     res.render('index');
+});
+app.get("/profile", requireLogin , (req, res) => {
+    console.log(req.session);
+    const { user } = req.session;
+    res.render("profile", {user});
 });
 app.get("/signup", (req, res) => {
     res.render("signup");
@@ -70,7 +89,9 @@ app.post("/login", async (req, res, next) => {
         bcrypt.compare(userpassword, newUser.userpassword, (err, result) =>{
             if(err) next(err);
             if(result){
-                res.render("profile", {user: newUser});
+                req.session.isVerified = true;
+                req.session.user = newUser.username;
+                res.redirect("/profile");
             }
             else{
                 res.send("Wrong username or password!");
