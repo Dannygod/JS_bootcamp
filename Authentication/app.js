39257d52ac3e4b2393310065fd358ec82b462efb
Danny Hsu <41147020S@gapps.ntnu.edu.tw>
2024-01-19 14:56:27 +0800

    0119 bcrypt

--- Authentication/app.js
+++ Authentication/app.js
@@ -3,6 +3,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const User = require('./model/user');
+const bcrypt = require('bcrypt');
+const saltRounds = 11;
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,17 +26,28 @@ app.get("/signup", (req, res) => {
 app.post("/signup", async (req, res, next) => {
     // console.log(req.body);
     const { username, userpassword } = req.body;
-    const newUser = new User({ username, userpassword});
+    const newUser = new User({ username, userpassword });
+    // encrypt password
     try{
-        await newUser.save()
-        .then(() =>{
-            res.send("Successfully signed up!");
-            console.log("User created!");
+        bcrypt.genSalt(saltRounds, async (err, salt) => {
+            if(err) next(err);
+            console.log(salt);
+            bcrypt.hash(newUser.userpassword, salt, async (err, hash)=>{
+                if(err) next(err);
+                newUser.userpassword = hash;
+                console.log(hash);
+                await newUser.save()
+                .then(() =>{
+                    res.send("Successfully signed up!");
+                    console.log("User created!");
+                })
+                .catch((e) =>{
+                    console.log(e);
+                    next(e);
+                });
+            })
         })
-        .catch((e) =>{
-            console.log(e);
-            next(e);
-        });
+        
     } catch(e){
         next(e);
     }
@@ -45,19 +58,18 @@ app.get("/login", (req, res) => {
 
 app.post("/login", async (req, res, next) => {
     const { username, userpassword } = req.body;
+    // check password
     try{
-        let newUser = await User.findOne({username})
-        .then((user) =>{
-            if(user.userpassword === userpassword && username === user.username){
+        let newUser = await User.findOne({username});
+        bcrypt.compare(userpassword, newUser.userpassword, (err, result) =>{
+            if(err) next(err);
+            if(result){
                 res.render("profile", {user: user});
             }
             else{
                 res.send("Wrong username or password!");
-            }})
-        .catch((e) =>{
-            console.log(e);
-            next(e);
-        });
+            }
+        })
     } catch(e){
         next(e);
     }
