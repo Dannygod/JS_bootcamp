904e1b4295a9b88a2e130225b2dcd862309c249a
Danny Hsu <danny539425@gmail.com>
2024-01-22 12:05:53 +0800

    0122 login route

--- Atlas/app.js
+++ Atlas/app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
+const authRoute = require('./routes/auth-route');
 mongoose.connect("mongodb+srv://Cluster91855:ZEV1T1ZUUV91@cluster91855.jprvfen.mongodb.net/", {
     useNewUrlParser: true, 
     useUnifiedTopology: true
 }).then(() => {
-    console.log('Connected to MongoDB');
+    console.log('Connected to mongo atlas');
 }).catch((err) => {
     console.log(err);
 });
@@ -15,9 +15,13 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+app.use('/auth', authRoute);
 
 app.get('/', (req, res) => {
     res.render("index.ejs")
+});
+app.get('/auth/login', (req, res) => {
+    res.render("login.ejs")
 });
 
 app.listen(3000, () => {
