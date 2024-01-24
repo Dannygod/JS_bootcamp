92d798893667303ca79e46196439d6f609f8869a
Danny Hsu <41147020S@gapps.ntnu.edu.tw>
2024-01-24 14:48:34 +0800

    0124 authcheck

--- Atlas/routes/profile-route.js
+++ Atlas/routes/profile-route.js
@@ -1,6 +1,15 @@
 const router = require('express').Router();
 
-router.get('/login', (req, res) => {
+const loginCheck = (req, res, next) => {
+    // console.log(req.user);
+    if(!req.isAuthenticated()){
+        res.redirect("/auth/login");
+    }
+    else{
+        next();
+    }
+}
+router.get('/', loginCheck, (req, res) => {
     res.render("profile", {user: req.user});
 });
 
